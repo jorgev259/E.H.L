1a24f737f69dec10403c63f4a11291e4af1bd47f
jorgev259 <jorgev259@outlook.es>
2018-09-25 06:19:30 -0600

    Update events.js

--- modules/welcome_begone/events.js
+++ modules/welcome_begone/events.js
@@ -22,8 +22,8 @@ module.exports = {
         let time = (new Date()).getTime()
         db.prepare('INSERT INTO timers (user,msg,guild,timestamp) VALUES (?,?,?,?)').run(msg.mentions.users.first().id, msg.id, msg.guild.id, time + (client.data.wconfig.minutes * 60000))
         setTimeout(check, client.data.wconfig.minutes * 60000, msg.mentions.users.first().id, db)
-        if (msg.content.length >= 600) msg.delete()
       }
+      if (msg.content.length >= 600) msg.delete()
     },
 
     async guildMemberRemove (client, db, member) {
